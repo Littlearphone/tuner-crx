8a99c63f8da6f8ac3cf7ca5e1047f217fa8b6da7
yaojiamin <yaojiamin@hikvision.com.cn>
2022-06-09 20:19:53 +0800

    fix：问题修复：功能、界面或代码审核等各种类型的问题
    修改一些页面的显示问题和分页加载问题

--- public/hack/bejson/json.js
+++ public/hack/bejson/json.js
@@ -1,4 +1,17 @@
-(function () {
-  const body = document.querySelector('body')
-  body.setAttribute('bejson', location.pathname)
+(function() {
+  const key = 'bejson-json'
+  chrome.storage.local.get([key], function(data) {
+    const config = data[key]
+    console.log(config)
+
+    function mark() {
+      const body = document.querySelector('body')
+      if (!body) {
+        return setTimeout(mark, 100)
+      }
+      body.setAttribute('bejson', location.pathname)
+    }
+
+    setTimeout(mark, 100)
+  })
 })()
