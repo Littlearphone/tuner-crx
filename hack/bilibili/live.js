d81c19ccf38d6fcb97c4a87d975078469fb9f2dc
Littlearphone <littlearphone@outlook.com>
2023-10-07 12:40:20 +0000

    Deploying to gh-pages from @ Littlearphone/tuner-crx@3744f1050e67ed6e771f3d9e0ac95d2cbf4f5a25 🚀

--- hack/bilibili/live.js
+++ hack/bilibili/live.js
@@ -4,25 +4,31 @@
   }
   console.log(`${window.logPrefix}%c ===> 开始初始化 Bilibili 脚本`, window.logStyle, '')
   $.bilibiliLoaded = {}
+
+  function detectPlaying(video, config) {
+    if (video.paused) {
+      return requestAnimationFrame(() => detectPlaying(video, config))
+    }
+    if (config.fullWebScreen && !document.body.classList.contains('player-full-win')) {
+      const event = new MouseEvent('dblclick', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
+      })
+      document.getElementById('awesome-pk-vm').dispatchEvent(event)
+      document.body.classList.add('player-full-win', 'over-hidden', 'hide-aside-area')
+    } else {
+      document.body.classList.remove('player-full-win', 'over-hidden', 'hide-aside-area')
+    }
+    // danmaku-item-container
+    requestAnimationFrame(() => window.dispatchEvent(new Event('resize')))
+  }
+
   chrome.runtime.onMessage.addListener(function (data, sender, callback) {
     console.log(`${window.logPrefix}%c ===> []~(￣▽￣)~* 脚本已准备就绪 `, window.logStyle, '')
     callback({msg: '[]~(￣▽￣)~*-script-injected'})
     const config = data.config || {}
     config.fullWebScreen = !config.hasOwnProperty('fullWebScreen') || config.fullWebScreen
-    $.detect('video', () => {
-      if (config.fullWebScreen && !document.body.classList.contains('player-full-win')) {
-        const event = new MouseEvent('dblclick', {
-          'view': window,
-          'bubbles': true,
-          'cancelable': true
-        })
-        document.getElementById('awesome-pk-vm').dispatchEvent(event)
-        document.body.classList.add('player-full-win', 'over-hidden', 'hide-aside-area')
-      } else {
-        document.body.classList.remove('player-full-win', 'over-hidden', 'hide-aside-area')
-      }
-      // danmaku-item-container
-      requestAnimationFrame(() => window.dispatchEvent(new Event('resize')))
-    })
+    $.detect('video', video => detectPlaying(video, config))
   })
 })(window.jQuery)
